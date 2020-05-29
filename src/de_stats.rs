16c193a55e2137682c696193c79c69f532656f85
Logan A Morrison <loanmorr@ucsc.com>
2020-05-28 23:58:00 -0700

    Adding a ODE statistics struct to keep track of stats

---
+++ src/de_stats.rs
@@ -0,0 +1,33 @@
+/// Statistics for a differential equation integration
+#[derive(Debug)]
+pub struct DEStatistics {
+    /// Number of steps taken
+    pub steps: usize,
+    /// Number of function evaluations
+    pub function_evals: usize,
+    /// Number of jacobian evaluations
+    pub jacobian_evals: usize,
+    /// Number of decompositions performed
+    pub decompositions: usize,
+    /// Number of linear solves performed
+    pub linear_solves: usize,
+    /// Number of accepted steps
+    pub accepts: usize,
+    /// Number of rejected steps
+    pub rejects: usize,
+}
+
+impl DEStatistics {
+    pub fn new() -> Self {
+        DEStatistics {
+            /// Number of steps taken
+            steps: 0,
+            function_evals: 0,
+            jacobian_evals: 0,
+            decompositions: 0,
+            linear_solves: 0,
+            accepts: 0,
+            rejects: 0,
+        }
+    }
+}
