105f7008b3d8b89831c328cb4c939af54ca1d31e
Logan A Morrison <loanmorr@ucsc.com>
2020-05-29 00:00:02 -0700

    Updating submodule declarations

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
+pub(crate) mod de_integrator;
+pub(crate) mod de_stats;
 pub(crate) mod linalg;
 pub(crate) mod radau;
--- src/radau.rs
+++ src/radau.rs
@@ -1,4 +1,7 @@
 pub(crate) mod alg;
 pub(crate) mod cache;
+pub(crate) mod continuous_output;
 pub(crate) mod dec;
+pub(crate) mod error_estimate;
+pub(crate) mod jacobian;
 pub(crate) mod sol;
