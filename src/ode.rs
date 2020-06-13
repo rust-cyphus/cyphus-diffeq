06151afb8804c8e48b14714822b9e2eab3f5b315
Logan A Morrison <loanmorr@ucsc.com>
2020-06-13 16:20:08 -0700

    Adding the 'ode' module to organize

---
+++ src/ode.rs
@@ -0,0 +1,17 @@
+pub mod algorithm;
+pub mod code;
+pub mod function;
+pub mod integrator;
+pub mod options;
+pub mod problem;
+pub mod solution;
+pub mod statistics;
+
+pub use algorithm::*;
+pub use code::*;
+pub use function::*;
+pub use integrator::*;
+pub use options::*;
+pub use problem::*;
+pub use solution::*;
+pub use statistics::*;
