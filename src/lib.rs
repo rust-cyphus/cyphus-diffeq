54ea14fc7b828a63cb3e9befaa9d116d83954b87
Logan A Morrison <loanmorr@ucsc.com>
2020-06-13 16:18:40 -0700

    Adding new modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
+pub mod dorm_prince;
 pub(crate) mod linalg;
-pub(crate) mod ode_algorithm;
-pub(crate) mod ode_integrator;
-pub(crate) mod ode_options;
-pub(crate) mod ode_prob;
-pub(crate) mod ode_solution;
-pub(crate) mod ode_stats;
-pub(crate) mod radau;
-
-use ndarray::prelude::*;
+pub mod ode;
+//pub mod radau;
+//pub(crate) mod rodas;
